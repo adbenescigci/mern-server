c88c11b9b0b29ed19e7f9ae170931b3f5b199197
Abdullah E CIGCI <cigcia1987@yahoo.com>
2022-03-23 16:31:16 +0400

    reverse

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ import auth from '../middleware/auth.js';
 const router = express.Router();
 
 //localhost:np5000/posts
-router.get('/', auth, getPosts);
+router.get('/', getPosts);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
